00adeb8420a022b74b5d4cb137f0a30f0a6da55c
Antônio Ximenes <almeid.ximenes@gmail.com>
2025-08-02 10:35:21 -0300

    Implementando o repository Campaign

--- src/container.ts
+++ src/container.ts
@@ -5,14 +5,16 @@ import { CampaignLeadsController } from "./controllers/CampaignLeadsController";
 import { GroupLeadsController } from "./controllers/GroupLeadsController";
 import { PrismaLeadsRepository } from "./repositories/prisma/PrismaLeadsRepository";
 import { PrismaGroupsRepository } from "./repositories/prisma/PrismaGroupsRepository";
+import { PrismaCampaignRepository } from "./repositories/prisma/PrismaCampaignRepository";
 
 // instanciando repositorios
 export const leadsRepository = new PrismaLeadsRepository()
 export const groupsRepository = new PrismaGroupsRepository()
-// cria uma instancia de leadsController para usar os metodos
+export const campaignRepository = new PrismaCampaignRepository()
+
 // criados na classe e usar - lo
 export const leadsController = new LeadsController(leadsRepository)
 export const groupsController = new GroupsController(groupsRepository)
-export const campaignController = new CampaignsController()
+export const campaignController = new CampaignsController(campaignRepository)
 export const campaignLeadsController = new CampaignLeadsController()
 export const groupLeadsController = new GroupLeadsController(groupsRepository, leadsRepository)
--- src/repositories/prisma/PrismaCampaignRepository.ts
+++ src/repositories/prisma/PrismaCampaignRepository.ts
@@ -3,40 +3,36 @@ import { CampaignRepository, CreateCampaign } from "../CampaignRepository";
 import { prisma } from "../../../prisma/database";
 
 export class PrismaCampaignRepository implements CampaignRepository {
+  async find(): Promise<Campaign[]> {
+    return prisma.campaign.findMany();
+  }
+  async findById(id: number): Promise<Campaign | null> {
+    return prisma.campaign.findUnique({
+      where: { id },
+      include: {
+        leads: {
+          include: {
+            lead: true,
+          },
+        },
+      },
+    });
+  }
+  async create(attributes: CreateCampaign): Promise<Campaign> {
+    return prisma.campaign.create({ data: attributes });
+  }
+  async update(id: number,attributes: Partial<CreateCampaign>): Promise<Campaign | null> {
+    const campaignExists = await prisma.campaign.findUnique({ where: { id } });
+    if (!campaignExists) return null;
 
-    async find(): Promise<Campaign[]>{
-        return prisma.campaign.findMany()
-    }
-    async findById(id: number): Promise<Campaign | null>{
-        return prisma.campaign.findUnique({ 
-            where: {id},
-            include: {
-                leads: {
-                    include: {
-                        lead: true
-                    }
-                }
-            }
-        })
-    }
-    async create(attributes: CreateCampaign): Promise<Campaign>{
-        return prisma.campaign.create({data: attributes})
-    }
-    async update(id: number, attributes: Partial<CreateCampaign>): Promise<Campaign | null>{
-        return prisma.campaign.update({
-            where: {id},
-            data: attributes,
-            include: {
-                leads: {
-                    include: {
-                        lead: true
-                    }
-                }
-            }
-        })
-    }
-    async delete(id: number): Promise<Campaign | null>{
-        return prisma.campaign.delete({where: {id}})
-    }
-
+    return prisma.campaign.update({
+      where: { id },
+      data: attributes,
+    });
+  }
+  async delete(id: number): Promise<Campaign | null> {
+    const campaignExists = await prisma.campaign.findUnique({ where: { id } });
+    if (!campaignExists) return null;
+    return prisma.campaign.delete({ where: { id } });
+  }
 }
